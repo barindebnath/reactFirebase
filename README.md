a5332364043dcae03eaa74344c3c14d6fc83bfb5
unknown <barindebnath@gmail.com>
2021-04-18 12:45:52 +0530

    build

--- README.md
+++
@@ -1,5 +0,0 @@
-## Fun Game
-
-Open this page in multiple device. (Desktop/Mobile/Tab)
-Now start typing in the given input field from any one device.
-You will see the change happening in all the device at the same time.
---
+++ precache-manifest.8dd3e7a2e944e7ab926f08f6c6400fcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a07562e223290f8cead99bf94a833832",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19eecd22ac0eaaca0f0a",
+    "url": "/static/css/main.ff7b65f4.chunk.css"
+  },
+  {
+    "revision": "18e3a288e975eed09a39",
+    "url": "/static/js/2.49d58d87.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/static/js/2.49d58d87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19eecd22ac0eaaca0f0a",
+    "url": "/static/js/main.c4e73323.chunk.js"
+  },
+  {
+    "revision": "3be40e8fea8b471fd549",
+    "url": "/static/js/runtime-main.753360d7.js"
+  }
+]);
