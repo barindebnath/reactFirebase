020d1660852f7dcc45b18eb5e4ed3ae83df31646
unknown <barindebnath@gmail.com>
2021-04-18 12:54:54 +0530

    change url

---
+++ precache-manifest.fbf30681ba2a30c2ec34596b4b35bbfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d32dedc7a9da45fd61e9ac52af58c01a",
+    "url": "/reactFirebase/index.html"
+  },
+  {
+    "revision": "3262632f52a1b6649c32",
+    "url": "/reactFirebase/static/css/main.82006776.chunk.css"
+  },
+  {
+    "revision": "fdd937b130529cc694d0",
+    "url": "/reactFirebase/static/js/2.96d9aa40.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/reactFirebase/static/js/2.96d9aa40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3262632f52a1b6649c32",
+    "url": "/reactFirebase/static/js/main.d9916871.chunk.js"
+  },
+  {
+    "revision": "0d9dbb8895b662fe6785",
+    "url": "/reactFirebase/static/js/runtime-main.30558821.js"
+  }
+]);
